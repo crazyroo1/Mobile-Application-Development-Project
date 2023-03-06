eb968b167bb7767ba452b2c2e534b15b059c8199
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-03-05 21:18:36 -0500

    New Link Dataclass, Forward to defeault web browser.

--- app/src/main/java/com/turnereison_sanjaybhadra_ethanbarton/mobileapplicationdevelopmentproject/Listable.kt
+++ app/src/main/java/com/turnereison_sanjaybhadra_ethanbarton/mobileapplicationdevelopmentproject/Listable.kt
@@ -1,7 +1,12 @@
 package com.turnereison_sanjaybhadra_ethanbarton.mobileapplicationdevelopmentproject
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import java.net.URL
 
 interface Listable {
     fun getPrimaryText(): String
@@ -26,6 +31,23 @@ data class File(override val name: String): FilesystemItem, Listable {
     override val tapAction: () -> Unit
         get() = {
             print("Tapped file named $name")
+        }
+}
+
+data class Link(override val name: String, val url: String, val context: Context): FilesystemItem, Listable {
+    override fun getPrimaryText(): String {
+        return name
+    }
+
+    override fun getSecondaryText(): String {
+        return "Link"
+    }
+
+    override val tapAction: () -> Unit
+        get() = {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+            print("Tapped Link named $name")
         }
 }
 
